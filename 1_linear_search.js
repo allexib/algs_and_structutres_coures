ac9f09aa3f8c30c9690e9b5948c94c9d18adcff0
allexib <ooomegasib@gmail.com>
2021-08-10 15:04:28 +0400

    1_linear_search.js

---
+++ 1_linear_search.js
@@ -0,0 +1,14 @@
+const array = [1,5,7,3,5,1,5,7,11,6,2]
+let count = 0
+function linearSearch(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        count += 1
+        if (array[i] === item) {
+            return i;
+        }
+    }
+    return null
+}
+
+console.log(linearSearch(array, 6))
+console.log('count = ', count)
